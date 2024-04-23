3992c25cfd2e9bbaf9f64dfb64a84ce76b1c2945
kobakk <davidyyhj@gmail.com>
2024-04-23 10:10:42 +0200

    Agrega pagina con php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?= 'Prueba' . '<br>'?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba Ramas Github</title>
+</head>
+<body>
+    
+</body>
+</html>
